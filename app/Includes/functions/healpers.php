5b89dac150e9d5bc24b018e72c1bd2771d9e30f2
zhaojjiang <zhaojjiang@foxmail.com>
2021-11-17 22:01:52 +0800

    处理 https

---
+++ app/Includes/functions/healpers.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!function_exists('is_https')) {
+    function is_https()
+    {
+        if (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) === 'on') {
+            return true;
+        }
+        if (!empty($_SERVER['SERVER_PORT']) && (int)$_SERVER['SERVER_PORT'] === 443) {
+            return true;
+        }
+        if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) === 'https') {
+            return true;
+        }
+        return false;
+    }
+}
