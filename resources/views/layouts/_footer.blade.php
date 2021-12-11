5b7f341d34bca05028a2d9c8d1de572f2f22c8e4
zhaojjiang <zhaojjiang@foxmail.com>
2021-12-11 20:22:18 +0800

    页脚添加公网安备

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,11 +1,19 @@
 <footer style="background: darkgrey;">
     <div class="col-10 offset-1 d-flex">
-        @if(!empty($ICP = \App\Models\Setting::get('ICP')))
-        <a href="https://beian.miit.gov.cn" class="nav-link text-white" target="_blank">{{ $ICP }}</a>
+        @if(!empty($beian_icp = \App\Models\Setting::get('beian_icp')))
+        <a href="https://beian.miit.gov.cn" class="nav-link text-white" target="_blank">{{ $beian_icp }}</a>
         @endif
 
         <span class="nav-link text-white">
             &copy;{{ date('Y', strtotime(\App\Models\Setting::get('date', date('Y-m-d')))) }}
         </span>
+
+        @if(!empty($beian_code = \App\Models\Setting::get('beian_code')) && !empty($beian_text = \App\Models\Setting::get('beian_text')))
+        <span class="nav-link text-white">
+            <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode={{ $beian_code }}" target="_blank">
+                <image src="{{ asset('assets/icon/beian.png') }}"></image> {{ $beian_text }}
+            </a>
+        </span>
+        @endif
     </div>
 </footer>
